5a3bc0cd6466a0412e9908234ff5a68d82a020b9
Jeff-GT <jefferson.guerrero@sdgku.edu>
2024-09-04 17:31:11 -0700

    Assignment #1 1st commit

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+let cha="Jeff";
+let act="play";
+let obj="volleyball"
+let past="ed";
+const emotion="happy"
+
+
+console.log(cha+" went out this morning.");
+console.log("He "+act+past+" "+obj+" with his friends.");
+console.log("This makes him very "+emotion+".");
+
+
